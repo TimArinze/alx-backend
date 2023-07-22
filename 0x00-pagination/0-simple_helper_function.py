03b5bfb40a1f12f500e963ce8ee2c1eb7617042a
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-23 00:00:02 +0100

    task

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index and end index
+    corresponding to the range of indexes to return in a list of pagination
+    """
+    start_index = page - 1
+    end_index = start_index + page_size
+    return (start_index, end_index)
