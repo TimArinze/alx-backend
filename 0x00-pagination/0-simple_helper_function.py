9062ac2cc2440d82efe057acac635373ecf35968
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-23 00:09:59 +0100

    task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,6 +10,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Returns a tuple of size two containing a start index and end index
     corresponding to the range of indexes to return in a list of pagination
     """
-    start_index = page - 1
+    start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
