ca0d5bbe1acb5b80885ba4ce051450a74a36fc28
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-27 03:26:46 +0100

    task

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,24 +3,22 @@
 Basic Dictionary
 """
 BaseCaching = __import__('base_caching').BaseCaching
-from typing import Any, Dict
 
 
 class BasicCache(BaseCaching):
     """A basic caching system"""
 
-    # def __init__(self):
-        # """Initializing"""
-        # super().__init__()
+    def __init__(self):
+        """Initializing"""
+        super().__init__()
 
-    def put(self, key: str, item: Any):
+    def put(self, key, item):
         """assign a dictionary self.cache_data (key and value)"""
         if key or item is not None:
             self.cache_data[key] = item
-        else:
-            pass
 
-    def get(self, key: str) -> Dict:
+    def get(self, key):
         """returns value of self.cache_data linked to key"""
         if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
+        return None
