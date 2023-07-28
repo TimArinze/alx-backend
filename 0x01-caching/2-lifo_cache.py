2c0b55a2664908bf4504fe079ce3dc351c1ddb39
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-28 13:46:32 +0100

    task

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,7 +16,8 @@ class LIFOCache(BaseCaching):
         if key and item:
             if key not in self.cache_data.keys():
                 if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                    last_key = next(reversed(self.cache_data))
+                    # last_key = next(reversed(self.cache_data))
+                    last_key = list(self.cache_data.keys())[-1]
                     print("DISCARD: {}".format(last_key))
                     self.cache_data.pop(last_key)
                 self.cache_data[key] = item
