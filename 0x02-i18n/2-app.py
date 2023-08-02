235a5476fb921d8aa12826fa11af9f8e5ce50c8e
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-02 12:44:05 +0100

    task

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,32 +3,36 @@
 """
 
 from flask import Flask, render_template, request
-from flask_babel import Babel,
+from flask_babel import Babel
 
 
 app = Flask(__name__)
-
-babel = Babel(app)
 
 
 class Config(object):
     """Config Class"""
     LANGUAGES = ["en", "fr"]
 
-    app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-    app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
+
+babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """Just rendering the template at /"""
     return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
