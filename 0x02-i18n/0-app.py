a9748f5c7340a0c378148968456e404d4e8a2f51
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-02 11:22:42 +0100

    task

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,4 +12,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
