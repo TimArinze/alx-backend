fae4b16d966c63b4cc9f6b87b626efeeeb83e674
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-02 05:05:12 +0100

    task

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Basic Babel setup
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config(object):
+    """Config Class"""
+    LANGUAGES = ["en", "fr"]
+    
+    app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+    app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+app.config.from_object(Config)
+
+@app.route('/')
+def index():
+    """Just rendering the template at /"""
+    return render_template('1-index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
